1e7e332ba71909ec5130bf09f6abbcb3a8faa54d
amitgupta7 <amit864@gmail.com>
2025-05-30 06:50:40 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,173 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id            = var.az_subscription_id
+  skip_provider_registration = true
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.az_name_prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.region
+  resource_group_name = var.az_resource_group
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.az_name_prefix}_pod-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.az_resource_group
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "pod_sg" {
+  name                = "${var.az_name_prefix}_pods-sg"
+  location            = var.region
+  resource_group_name = var.az_resource_group
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "${var.client_ip}"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "pod_ip" {
+  name                = "${var.az_name_prefix}_jumpbox_ip"
+  location            = var.region
+  resource_group_name = var.az_resource_group
+  allocation_method   = "Static"
+  domain_name_label   = "${var.az_name_prefix}-jumpbox"
+}
+
+resource "azurerm_network_interface" "pod_nic" {
+  name                = "${var.az_name_prefix}_jumpbox_nic"
+  location            = var.region
+  resource_group_name = var.az_resource_group
+  ip_configuration {
+    name                          = "${var.az_name_prefix}_jumpbox_ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.2.21"
+    public_ip_address_id          = azurerm_public_ip.pod_ip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg" {
+  network_interface_id      = azurerm_network_interface.pod_nic.id
+  network_security_group_id = azurerm_network_security_group.pod_sg.id
+}
+
+resource "azurerm_linux_virtual_machine" "jumpbox-vm" {
+  name                  = "${var.az_name_prefix}-jumpbox-vm"
+  network_interface_ids = [azurerm_network_interface.pod_nic.id]
+  //variables
+  location            = var.region
+  resource_group_name = var.az_resource_group
+  size                = var.vm_size
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = var.os_publisher
+    offer     = var.os_offer
+    sku       = var.os_sku
+    version   = var.os_version
+  }
+  admin_username                  = var.azuser
+  admin_password                  = var.azpwd
+  disable_password_authentication = false
+
+}
+
+
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "${var.az_name_prefix}-aks"
+#   kubernetes_version  = var.kubernetes_version
+#   location            = var.region
+#   resource_group_name = var.az_resource_group
+#   dns_prefix          = var.az_name_prefix
+#   private_cluster_public_fqdn_enabled = true
+
+#   default_node_pool {
+#     name                = "system"
+#     node_count          = var.min_node_count
+#     vm_size             = var.node_vm_size
+#   }
+
+#     linux_profile {
+#     admin_username = "azureuser"
+#     ssh_key {
+#       key_data = file("~/.ssh/id_rsa.pub")
+#     }
+#     }
+
+#     network_profile {
+#     network_plugin    = "kubenet"
+#     load_balancer_sku = "standard"
+#   }
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+
+#   tags = {
+#     Environment = "dev"
+#   }
+# }
+
+resource "null_resource" "post_provisioning" {
+    
+  triggers = {
+    build_number = "${timestamp()}"
+  }  
+  depends_on = [azurerm_linux_virtual_machine.jumpbox-vm]
+  connection {
+    type     = "ssh"
+    user     = var.azuser
+    password = var.azpwd
+    host     = azurerm_public_ip.pod_ip.fqdn
+  }
+#   provisioner "local-exec" {
+#     command = "az aks get-credentials --resource-group ${var.az_resource_group} --name ${azurerm_kubernetes_cluster.aks.name} --file config.aks --overwrite-existing"
+#   }
+
+   provisioner "file" {
+    source = "config.aks"
+    destination = "/home/${var.azuser}/.kube_config"
+    
+  }
+    provisioner "remote-exec" {
+    inline = [
+    #   "mkdir -p /home/${var.azuser}/.kube && mv /home/${var.azuser}/.kube_config /home/${var.azuser}/.kube/config && chmod 600 /home/${var.azuser}/.kube/config",
+      "ls -lart"
+     ]
+  }
+}
+
+ 
+
+# resource "null_resource" "setup_datastores" {
+#   depends_on = [null_resource.post_provisioning]
+#   provisioner "local-exec" {
+#     command = "kubectl get pods"
+#   }
+# }
+
+output "ssh_credentials" {
+  value = "ssh -L 8800:localhost:8800 ${var.azuser}@${azurerm_public_ip.pod_ip.fqdn} \nwith password: ${var.azpwd}"
+}
---
+++ var.tf
@@ -0,0 +1,100 @@
+variable "az_subscription_id" {
+  description = "azure subscription id"
+  type        = string
+}
+
+variable "region" {
+  default = "westus2"
+}
+
+variable "vm_size" {
+  default = "Standard_B1s"
+}
+
+
+variable "os_disk_size_in_gb" {
+  default = 1024
+}
+
+
+variable "os_publisher" {
+  default = "RedHat"
+}
+
+variable "os_offer" {
+  default = "RHEL"
+}
+
+variable "os_sku" {
+  default = "8_5"
+}
+
+variable "os_version" {
+  default = "latest"
+}
+
+variable "azpwd" {
+  description = "common vm password, 16 characters containg --> [chars-alpha-num-special-char]"
+  default = "1qaz!QAZ1qaz!QAZ"
+}
+
+variable "client_ip" {
+    description = "Optional, add the client public IP address to whitelist ssh communication"
+    default = "*"
+    type = string
+}
+
+
+variable "azuser" {
+  default = "azuser"
+}
+
+
+variable "az_resource_group" {
+  description = "resource group name to create these resources"
+}
+
+variable "az_name_prefix" {
+  description = "prefix to add to resource names"
+  default     = "azure-tf-aks"
+}
+
+variable "node_vm_size" {
+  default = "Standard_D4_v2"
+}
+
+variable "min_node_count" {
+  default     = "2"
+  description = "AKS min nodes"
+}
+
+variable "max_node_count" {
+  default     = "4"
+  description = "AKS max nodes"
+}
+
+variable "kubernetes_version" {
+  default     = "1.33"
+  description = "Kubernetes version"
+}
+
+
+variable "X_API_Secret" {
+  type        = string
+  description = "SAI API secret"
+}
+  
+variable "X_API_Key" {
+  type        = string
+  description = "SAI API key"
+}
+
+variable "X_TIDENT" {
+  type        = string
+  description = "SAI Tenant ID"
+}
+
+variable "pod_owner" {
+  type        = string
+  description = "POD Owner Email, must be SAI tenant admin"
+}
